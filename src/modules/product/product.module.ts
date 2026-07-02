2d234093ae89265b7d232730cb4642eb1942cb83
KallMeMoh <kallmemoh@gmail.com>
2026-07-03 00:15:13 +0300

    add missing dependency.

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './product.service';
 import { ProductsController } from './product.controller';
 import { ProductsRepository } from './product.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './entities/product.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService, ProductsRepository],
 })
