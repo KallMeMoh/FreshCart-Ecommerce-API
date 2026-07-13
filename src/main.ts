5c7f701bf40606bb8dd875bb16073fa89eb7a049
KallMeMoh <kallmemoh@gmail.com>
2026-07-13 13:35:26 +0300

    change: using logger for bootstrap-caught errors.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from './modules/config/config.service';
 import cookieParser from 'cookie-parser';
 
+const logger = new Logger('Bootstrap');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -22,13 +23,12 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.port;
 
-  const logger = new Logger('Bootstrap');
   await app.listen(port, () =>
     logger.log('Server is up and running on port 3000'),
   );
 }
 
 bootstrap().catch((err) => {
-  console.error(err);
+  logger.error(err);
   process.exit(1);
 });
